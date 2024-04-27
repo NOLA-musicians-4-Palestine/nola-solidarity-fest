f2a84a0c23ef554cc210fdb213bc830ae152665e
Simon Moushabeck <simonmoush@gmail.com>
2024-04-26 21:19:47 -0500

    try to get a sheet

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 	//document.querySelector("#today").scrollTop = 0;
 	//scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+	const req = new XMLHttpRequest();
+	req.addEventListener("load", reqListener);
+	req.open("GET", "https://docs.google.com/spreadsheets/d/1pW2hj9qozqU1_ySxkmj9juo7350a-ADYyQIUW2G2nL0/edit?usp=sharing");
+	req.send();
+
+	document.querySelector("#more-info-link").addEventListener("click", function(){
+		document.querySelector("#more-info").scrollIntoView({behavior: "smooth", block: "start"});
+	});
 });
 
-document.querySelector("#more-info-link").addEventListener("click", function(){
-	console.log("simon");
-	document.querySelector("#more-info").scrollIntoView({behavior: "smooth", block: "start"});
-});
-
+function reqListener() {
+  document.querySelector("#response").innerHTML = this.responseText;
+}
